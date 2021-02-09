e0465a2ac689f46a5aa162d87586d79f6484cf94
Veronika <veronikakuchtova95@gmail.com>
2021-02-09 18:14:51 +0100

    Task Validator

--- myProjectOneMany/app/Http/Controllers/TaskController.php
+++ myProjectOneMany/app/Http/Controllers/TaskController.php
@@ -7,41 +7,64 @@ use Illuminate\Http\Request;
 use App\Employee;
 use App\Task;
 use App\Typology;
+use Illuminate\Support\Facades\Validator;
 
 class TaskController extends Controller
 {
+  // Index
   public function taskIndex() {
 
     $tasks = Task::all();
     return view('pages.task-index', compact('tasks'));
   }
 
+  // Show
   public function taskShow($id) {
 
     $task = Task::findOrFail($id);
     return view('pages.task-show', compact('task'));
   }
 
+  // Create
   public function taskCreate() {
 
     $employees = Employee::all();
     $typologies = Typology::all();
     return view('pages.task-create', compact('employees', 'typologies'));
   }
+  // Store
   public function taskStore(Request $request) {
 
     $data = $request -> all();
+
+    Validator::make($data, [
+            'title' => 'required|min:3',
+            'description' => 'required|min:5|max:20',
+            'priority' => 'required|integer|between:1,5',
+        ]) -> validate();
+
     $task = Task::make($request -> all());
     $employee = Employee::findOrFail($data['employee_id']);
     $task -> employee() -> associate($employee);
     $task -> save();
 
-    $typologies = Typology::findOrFail($data['typologies']);
-    $task -> typologies() -> attach($typologies);
+    // $typologies = Typology::findOrFail($data['typologies']);
+    // $task -> typologies() -> attach($typologies);
+    if (array_key_exists('typologies', $data)) {
+
+      $typologies = Typology::findOrFail($data['typologies']);
+      $task -> typologies() -> attach($typologies);
+
+    }else {
+
+      $task -> typologies() -> attach([]);
+
+    }
 
     return redirect() -> route('task-index');
   }
 
+  // Edit
   public function taskEdit($id) {
 
     $task = Task::findOrFail($id);
@@ -50,9 +73,16 @@ class TaskController extends Controller
 
     return view('pages.task-edit', compact('employees', 'typologies', 'task'));
   }
+  // Update
   public function taskUpdate(Request $request, $id) {
 
     $data = $request -> all();
+
+    Validator::make($data, [
+            'title' => 'required|min:3',
+            'description' => 'required|min:5|max:20',
+            'priority' => 'required|integer|between:1,5',
+        ]) -> validate();
 
     $employee = Employee::findOrFail($data['employee_id']);
     $task = Task::findOrFail($id);
@@ -60,8 +90,16 @@ class TaskController extends Controller
     $task -> employee() -> associate($employee);
     $task -> save();
 
-    $typologies = Typology::findOrFail($data['typologies']);
-    $task -> typologies() -> sync($typologies);
+    if (array_key_exists('typologies', $data)) {
+
+      $typologies = Typology::findOrFail($data['typologies']);
+      $task -> typologies() -> sync($typologies);
+
+    }else {
+
+      $task -> typologies() -> sync([]);
+
+    }
 
     return redirect() -> route('task-index');
   }
--- myProjectOneMany/app/Http/Controllers/TypologyController.php
+++ myProjectOneMany/app/Http/Controllers/TypologyController.php
@@ -10,23 +10,27 @@ use Illuminate\Support\Facades\Validator;
 
 class TypologyController extends Controller
 {
+  // Index
   public function typologyIndex() {
 
     $typologies = Typology::all();
     return view('pages.typology-index', compact('typologies'));
   }
 
+  // Show
   public function typologyShow($id) {
 
     $typology = Typology::findOrFail($id);
     return view('pages.typology-show', compact('typology'));
   }
 
+  // Create
   public function typologyCreate() {
 
     $tasks = Task::all();
     return view('pages.typology-create', compact('tasks'));
   }
+  // Store
   public function typologyStore(Request $request) {
 
     $data = $request -> all();
@@ -36,14 +40,25 @@ class TypologyController extends Controller
             'description' => 'required|min:5|max:20'
         ]) -> validate();
 
-    $newTypology = Typology::create($data);
-    $tasks = Task::findOrFail($data['tasks']);
-    $newTypology -> tasks() -> attach($tasks);
+    $typology = Typology::create($data);
 
+    // $tasks = Task::findOrFail($data['tasks']);
+    // $typology -> tasks() -> attach($tasks);
+    if (array_key_exists('tasks', $data)) {
+
+      $tasks = Task::findOrFail($data['tasks']);
+      $typology -> tasks() -> attach($tasks);
+
+    }else {
+
+      $typology -> tasks() -> attach([]);
+
+    }
 
     return redirect() -> route('typology-index');
   }
 
+  // Edit
   public function typologyEdit($id) {
 
     $typology = Typology::findOrFail($id);
@@ -51,6 +66,7 @@ class TypologyController extends Controller
 
     return view('pages.typology-edit', compact('typology', 'tasks'));
   }
+  // Update
   public function typologyUpdate(Request $request, $id) {
 
     $data = $request -> all();
